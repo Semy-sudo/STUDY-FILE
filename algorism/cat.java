e20642e6cc5af2eee3e936f833f8def7cb49f304
Yein <dpdls0603@naver.com>
2020-04-27 12:13:19 +0900

    4/27

---
+++ algorism/cat.java
@@ -0,0 +1,13 @@
+package M1;
+
+public class cat {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
