de5ded556a30df4e7880d912d3fdd0a6abdf0040
Yein <dpdls0603@naver.com>
2020-05-04 15:46:57 +0900

    5/4

---
+++ algorism/백준3015.java
@@ -0,0 +1,22 @@
+package M2;
+
+import java.io.IOException;
+
+public class 백준3015 {
+
+	public static void main(String[] args) throws IOException {
+		
+
+	}
+
+}
+
+class Pair{
+	int x;//키
+	int y;//키가 같은 사람
+	
+	public Pair(int x,int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
